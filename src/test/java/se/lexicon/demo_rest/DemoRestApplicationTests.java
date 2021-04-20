fca76c2060f37f926138a31fea9a02e2d385d33b
asha joseph <ashajo183@gmail.com>
2021-04-20 15:29:57 +0200

    Initial commit

---
+++ src/test/java/se/lexicon/demo_rest/DemoRestApplicationTests.java
@@ -0,0 +1,13 @@
+package se.lexicon.demo_rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
